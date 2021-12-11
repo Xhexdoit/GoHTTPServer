85013cd8ca71edf51bfcdde86454a13db8bc610b
Cristina Hidalgo Cobo <87225960+Xhexdoit@users.noreply.github.com>
2021-12-11 20:37:49 +0100

    Create serverhttp.go

---
+++ serverhttp.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+  "log"
+	"net/http"
+)
+
+func handler(w http.ReponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello, %s, ¡This is a server!", r.URL.Path[1:])
+}
+
+func main(){
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+  if err := http.ListenAndServe(":8080", nil) ; err != nil{
+  log.Fatal(err)
+  }
+}
