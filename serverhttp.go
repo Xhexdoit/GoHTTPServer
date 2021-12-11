7b8ffd24d0da8d2603cdd40725465b717f1c0999
Cristina Hidalgo Cobo <87225960+Xhexdoit@users.noreply.github.com>
2021-12-11 20:46:21 +0100

    Update serverhttp.go

--- serverhttp.go
+++ serverhttp.go
@@ -7,12 +7,12 @@ import (
 )
 
 func handler(w http.ReponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello, %s, ¡This is a server!", r.URL.Path[1:])
+fmt.Fprintf(w, "Hello, %s, ¡This is a server!", r.URL.Path[1:])
 }
 
 func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+http.HandleFunc("/", handler)
+http.ListenAndServe(":8080", nil)
   if err := http.ListenAndServe(":8080", nil) ; err != nil{
   log.Fatal(err)
   }
